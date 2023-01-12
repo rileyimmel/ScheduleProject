1a33a06cb0a37ecbb2ef05975fe2b707527f1ec1
rileyimmel <ypq8st@virginia.edu>
2023-01-12 17:35:34 -0500

    Begin creating class for importing courses from api

---
+++ src/main/java/edu/virginia/cs/immelScheduleProject/AddCourses.java
@@ -0,0 +1,16 @@
+package edu.virginia.cs.immelScheduleProject;
+
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.net.URL;
+import java.net.URLConnection;
+import java.util.List;
+
+public class AddCourses {
+
+    public List<Course> getCourses(){
+        //TODO: get courses from API
+        return null;
+    }
+}
